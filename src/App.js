effb36d4c979396be2c0b35e5d8f6b67d2ee8f83
Anand Shukla <anands2807@gmail.com>
2021-12-03 16:05:06 +0530

    components added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World.</h1>
+      <Profile/>
+      <Login/>
     </div>
   );
 }
