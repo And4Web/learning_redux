7efd95057da623d85f571d0877ee5e9160eaf375
Anand Shukla <anands2807@gmail.com>
2021-12-03 18:25:33 +0530

    reducers added.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './features/user';
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    user: userReducer,
+  }
 })
 
 ReactDOM.render(
