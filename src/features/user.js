019f516ae58917bbb77b3a1ce84de3790664d4cf
Anand Shukla <anands2807@gmail.com>
2021-12-04 19:50:58 +0530

    fresh changes.

--- src/features/user.js
+++ src/features/user.js
@@ -10,4 +10,8 @@ export const userSlice = createSlice({
   }
 })
 
+export const {login} = userSlice.actions;
+
 export default userSlice.reducer;
+
+//we are creating user slices here in user reducer, which creates a state named user with value as initial state. we can access it using state.user.value in components and passing it as user.name, user.age, user.email etc.
