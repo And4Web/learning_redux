b1baa8d0a5f79dd9ca7c3aac37acf13376f6c7cc
Anand Shukla <anands2807@gmail.com>
2021-12-05 16:57:00 +0530

    dispatch added to the code.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {login} from '../features/user';
 
 function Login() {
+  const dispatch = useDispatch();
   return (
     <div>
-      <button>Login</button>
+      <button onClick={()=>{dispatch(login({name: "Anand", age: 30, email: "anand@gmail.com"}))}}>Login</button>
     </div>
   )
 }
